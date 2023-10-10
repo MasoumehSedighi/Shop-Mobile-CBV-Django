791100887513fbb686728e6c5c40c4a89e01d8fc
MasoumehSedighi <mas_cutt4@yahoo.com>
2023-10-10 17:14:30 +0330

    login

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -17,12 +17,15 @@ class loginView(views.View):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
-            email = form.cleaned_data("email")
-            password = form.cleaned_data("password")
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
             user = authenticate(request, email=email, password=password)
             if user is not None:
                 login(request, user)
                 redirect('product-list')
             else:
                 return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form':form})   
+
+
 
